adc8b9fb94371b353faa0574bb305c3e6d349b90
l0u0h0 <l0u0h0@gmail.com>
2022-03-23 20:38:55 +0900

    Router

---
+++ react-router-example/src/App.js
@@ -0,0 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import About from "./pages/About";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Profile" element={<Profile />} />
+        <Route path="/About" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
