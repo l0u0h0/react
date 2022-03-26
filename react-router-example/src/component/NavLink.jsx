373e0ffac85c22f679f826cfd70e486dc782615e
l0u0h0 <l0u0h0@gmail.com>
2022-03-26 16:33:22 +0900

    NavLink isactive

--- react-router-example/src/component/NavLink.jsx
+++ react-router-example/src/component/NavLink.jsx
@@ -1,45 +1,57 @@
 import { NavLink, useLocation } from "react-router-dom";
 
-const style = ({ isActive }) => ({
-  color: isActive ? "green" : "blue",
-});
-// const { location } = useLocation();
+// let { location } = useLocation();
 // const obj = new URLSearchParams(location);
 
 export default function NavLinks() {
-  const { search } = useLocation();
+  const search = useLocation();
+  console.log(search);
   const obj = new URLSearchParams(search);
+  console.log(obj);
   return (
     <ul>
       <li>
-        <NavLink to="/" style={style}>
+        <NavLink
+          to="/"
+          style={({ isActive }) => ({
+            color: isActive ? "green" : "blue",
+          })}
+        >
           Home
         </NavLink>
       </li>
       <li>
-        <NavLink to="/Profile/" style={style}>
+        <NavLink
+          to="/Profile/"
+          style={({ isActive }) => ({
+            color: isActive ? "green" : "blue",
+          })}
+        >
           Profile
         </NavLink>
       </li>
       <li>
-        <NavLink to="/Profile/2" style={style}>
+        <NavLink
+          to="/Profile/2"
+          style={({ isActive }) => ({
+            color: isActive ? "green" : "blue",
+          })}
+        >
           Profile/2
         </NavLink>
       </li>
       <li>
-        <NavLink
-          to="/About"
-          style={style}
-          // isActive={() => {
-          //   console.log(search);
-          //   return search === " ";
-          // }}
-        >
+        <NavLink to="/About/" style={({ isActive, search }) => ({})}>
           Abvout
         </NavLink>
       </li>
       <li>
-        <NavLink to="/About?name=uhan" style={style}>
+        <NavLink
+          to="/About?name=uhan/"
+          style={({ isActive }) => ({
+            color: isActive ? "green" : "blue",
+          })}
+        >
           About?name=uihan
         </NavLink>
       </li>
